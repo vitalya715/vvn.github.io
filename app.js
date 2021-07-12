1d57d7006af379461ec6448db6a8bd1850d2f209
vitalya715 <59776946+vitalya715@users.noreply.github.com>
2021-07-12 13:34:59 +0300

    Залил

---
+++ app.js
@@ -0,0 +1,58 @@
+$(function () {
+    // Fixed Heder
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle")
+
+
+    $(window).on("scroll load resize", function(){
+        introH = intro.innerHeight();
+
+        scrollPos=$(this).scrollTop();
+        if(scrollPos >= 100) {
+            header.addClass("fixed");
+        } else{
+            header.removeClass("fixed");
+        }
+
+    } );
+
+    // Smooth Scroll
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+        let elementId = $(this).data('scroll');
+        let elementOffset =$(elementId).offset().top;
+
+        nav.removeClass("show");
+        $("html, body").animate({
+            scrollTop:elementOffset -50
+        }, 700 )
+    });
+
+    // Nav Toggle
+    
+
+    navToggle.on("click", function (event) {
+         event.preventDefault();
+
+         nav.toggleClass("show")
+
+    });
+
+    // Reviews https://kenwheeler.github.io/slick/
+
+    let slider = $("#reviewsSlider");
+        slider.slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: false,
+            arrows: false
+      });
+    
+
+});
